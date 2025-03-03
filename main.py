04470c7f465cafeab93ffc73961a705ca84f6c25
Billy <billyzhaodong@gmail.com>
2025-03-03 12:33:47 +0000

    async serial client, need to test with board

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ import sys
 from PyQt6 import QtWidgets
 from LED_controller_UI import MainApp  # Importing the UI class
 
+from pymodbus.client import AsyncModbusSerialClient
+client = AsyncModbusSerialClient(port= "COM4", timeout=1, baudrate=115200, bytesize=8, stopbits=1, parity="N")
+client.connect()
+
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
     window = MainApp()
